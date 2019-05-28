3803635ec7ad713ae841b917567198dec375cde2
Sarah Zurmühle <s.zurmuehle@hispeed.ch>
2019-05-28 10:45:21 +0200

    Create Unit test for privacy settings JObject initialization in dashboard project

---
+++ KaVE.VS.Dashboard.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KaVE.VS.Dashboard.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KaVE.VS.Dashboard.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("207ecd8d-d426-49ba-b7f7-12fae9dcd735")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
